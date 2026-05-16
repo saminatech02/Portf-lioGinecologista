695584210fc86da888acf48cd6bf8ce44f24876e
saminatech02 <saminatechh@gmail.com>
2026-05-15 23:30:15 -0300

    fix: Novas mudanças

--- portfolio-gineco-app/src/App.tsx
+++ portfolio-gineco-app/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Home from "./pages/Home";
 import "./styles/global.css";
-import { Analytics } from "@vercel/analytics/next"
 
 const App: React.FC = () => {
   return <Home />;
