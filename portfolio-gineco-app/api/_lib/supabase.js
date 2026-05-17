99ae11fb5910a8aae17c6847c9cf292578317f6c
saminatech02 <saminatechh@gmail.com>
2026-05-17 09:34:34 -0300

    fix: Nome vindo do env

--- portfolio-gineco-app/api/_lib/supabase.js
+++ portfolio-gineco-app/api/_lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseServiceRoleKey = process.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseServiceRoleKey) {
   throw new Error(
